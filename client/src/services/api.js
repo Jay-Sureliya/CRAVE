a96a49296fa1c737aca1d922dcc820d0334d4156
jay <jsureliya732@rku.ac.in>
2026-05-02 00:52:54 +0530

    fix some depo problem

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000" || "https://crave-qozy.onrender.com",
+  baseURL: "https://crave-qozy.onrender.com",
   withCredentials: true,
 });
 
--- server/app/main.py
+++ server/app/main.py
@@ -86,7 +86,7 @@ app = FastAPI(title="Crave API")
 # --- CORS CONFIGURATION ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173", "http://localhost:8000"], 
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173", "http://localhost:8000" , "https://crave-1-xeuc.onrender.com"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
